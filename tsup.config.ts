1ba94d6262980b5d98e46baa89d946b1fa83539b
David Plugge <59972093+david-plugge@users.noreply.github.com>
2024-02-21 14:48:57 +0100

    add shebang to cli script

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,15 +1,24 @@
 import { defineConfig } from 'tsup';
 import { name, version } from './package.json';
 
-export default defineConfig({
-	entry: ['src/vite.ts', 'src/cli.ts'],
-	format: ['esm'],
-	dts: {
-		entry: ['src/vite.ts']
+export default defineConfig([
+	{
+		entry: ['src/vite.ts', 'src/cli.ts'],
+		format: ['esm'],
+		dts: true,
+		clean: true
 	},
-	clean: true,
-	define: {
-		PKG_NAME: JSON.stringify(name),
-		PKG_VERSION: JSON.stringify(version)
+	{
+		entry: ['src/cli.ts'],
+		format: ['esm'],
+		dts: false,
+		clean: true,
+		banner: {
+			js: '#!/usr/bin/env node'
+		},
+		define: {
+			PKG_NAME: JSON.stringify(name),
+			PKG_VERSION: JSON.stringify(version)
+		}
 	}
-});
+]);
